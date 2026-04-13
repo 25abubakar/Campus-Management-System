d077a20914cffae299f3dbba3694ec52aa761b07
LALTECH\abubakar_291 <muhammadabubakarkhan1234@gmail.com>
2026-04-13 11:13:43 +0500

    attendance process

--- Campus Management System/Controllers/AttendanceController.cs
+++ Campus Management System/Controllers/AttendanceController.cs
@@ -14,35 +14,101 @@ namespace Campus_Management_System.Controllers
             _context = context;
         }
 
-        // LIST PAGE
-        public IActionResult Index()
+        public IActionResult Index(int? courseId, DateTime? date)
         {
-            var data = _context.Attendances
-                .Include(a => a.Student)
+            var query = _context.Attendances
+                .Include(a => a.Student).ThenInclude(s => s.Person)
                 .Include(a => a.Course)
-                .ToList();
+                .AsQueryable();
 
-            return View(data);
-        }
+            if (courseId.HasValue)
+                query = query.Where(a => a.CourseId == courseId);
 
-        // CREATE PAGE
-        public IActionResult Create()
-        {
-            ViewBag.Students = _context.Students
-                .Include(s => s.Person)
-                .ToList();
+            if (date.HasValue)
+                query = query.Where(a => a.Date.Date == date.Value.Date);
 
             ViewBag.Courses = _context.Courses.ToList();
 
+            return View(query.ToList());
+        }
+
+        public IActionResult TakeAttendance()
+        {
+            ViewBag.Courses = _context.Courses.ToList();
             return View();
         }
 
-        // SAVE ATTENDANCE
         [HttpPost]
-        public IActionResult Create(Attendance attendance)
+        public IActionResult LoadStudents(int CourseId, DateTime Date)
         {
-            _context.Attendances.Add(attendance);
+            bool alreadyMarked = _context.Attendances
+                .Any(a => a.CourseId == CourseId && a.Date.Date == Date.Date);
+
+            if (alreadyMarked)
+            {
+                TempData["Error"] = "Attendance already marked for this date!";
+                return RedirectToAction("Index");
+            }
+
+            var students = _context.StudentCourses
+                .Include(sc => sc.Student).ThenInclude(s => s.Person)
+                .Where(sc => sc.CourseId == CourseId)
+                .Select(sc => new StudentAttendanceRow
+                {
+                    StudentId = sc.StudentId,
+                    StudentName = sc.Student.Person.Name
+                })
+                .ToList();
+
+            AttendanceVM vm = new AttendanceVM
+            {
+                CourseId = CourseId,
+                Date = Date,
+                Students = students
+            };
+
+            return View("MarkAttendance", vm);
+        }
+
+        [HttpPost]
+        public IActionResult SaveAttendance(AttendanceVM vm)
+        {
+            var allStudents = _context.StudentCourses
+                .Where(sc => sc.CourseId == vm.CourseId)
+                .Select(sc => sc.StudentId)
+                .ToList();
+
+            foreach (var studentId in allStudents)
+            {
+                bool isPresent = vm.PresentStudentIds.Contains(studentId);
+
+                Attendance a = new Attendance()
+                {
+                    StudentId = studentId,
+                    CourseId = vm.CourseId,
+                    Date = vm.Date,
+                    IsPresent = isPresent
+                };
+
+                _context.Attendances.Add(a);
+            }
+
             _context.SaveChanges();
+
+            TempData["Success"] = "Attendance saved successfully!";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var att = _context.Attendances.Find(id);
+
+            if (att != null)
+            {
+                _context.Attendances.Remove(att);
+                _context.SaveChanges();
+            }
+
             return RedirectToAction("Index");
         }
     }
---
+++ Campus Management System/Models/AttendanceViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Campus_Management_System.Models
+{
+    public class AttendanceVM
+    {
+        public int CourseId { get; set; }
+        public DateTime Date { get; set; }
+
+        public List<int> PresentStudentIds { get; set; } = new();
+
+        public List<StudentAttendanceRow> Students { get; set; } = new();
+    }
+
+    public class StudentAttendanceRow
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+}
